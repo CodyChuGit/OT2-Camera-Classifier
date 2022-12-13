180dc14d47c7b65a29d727781d72397ee7a01456
Cody Chu <120514265+CodyChuGit@users.noreply.github.com>
2022-12-13 17:02:51 -0500

    Delete Use_Trained_Model.py

--- Use_Trained_Model.py
+++
@@ -1,8 +0,0 @@
-from tensorflow.keras.models import load_model
-model.save(os.path.join('/Users/codychu/Projects/Machine_Learning/models/labwareclassifier.h5'))
-new_model = load_model('/Users/codychu/Projects/Machine_Learning/models/labwareclassifier.h5')
-new_model.predict(np.expand_dims(resize/255, 0))
-if yhat > 0.5: 
-    print(f'Predicted class is Labware')
-else:
-    print(f'Predicted class is Empty')
